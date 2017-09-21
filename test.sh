cb2d772b7fae4e971bd51443306c491d7ecd4aff
liao-bo <55465967@qq.com>
2017-09-21 17:18:12 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,11 @@
 #sed -i 's/\(-k","\).*\("]$\)/\1'aaaaaa'\2/' bx-kube-replicat.yaml
 OS_VERSION="LINUX"
 if [[ $OS_VERSION -eq "MAC" ]];then
-        sh <(curl -fsSL https://clis.ng.bluemix.net/install/osx)
+        #sh <(curl -fsSL https://clis.ng.bluemix.net/install/osx)
+        echo "111"
     elif [[ $OS_VERSION -eq "LINUX" ]];then
-        sh <(curl -fsSL https://clis.ng.bluemix.net/install/linux)
+        #sh <(curl -fsSL https://clis.ng.bluemix.net/install/linux)
+        echo "222"
     else
         exit 1
     fi
